ed32c85ea4149e9e5e080e2befad99f9c48d6638
云落 <sp91@foxmail.com>
2026-04-26 08:35:58 +0800

    chore: bump version to 1.3.0.0

--- src/process_monitor.c
+++ src/process_monitor.c
@@ -7,11 +7,11 @@
 #include <direct.h>
 #include <ctype.h>
 
-#define VERSION "1.2.0.2"
+#define VERSION "1.3.0.0"
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 2
+#define VERSION_MINOR 3
 #define VERSION_PATCH 0
-#define VERSION_BUILD 2
+#define VERSION_BUILD 0
 
 void get_version_string(char* buffer, size_t size) {
     int len = snprintf(buffer, size, "%d.%d.%d.%d",
